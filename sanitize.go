b3fea5384099cd15926bb49998321a09907ce362
Savannah Ryan <savannah@SavannahRyan-MBP.local>
2020-10-16 09:03:42 -0600

    Added RedactAll function, still needs a little work with other functions.

--- sanitize.go
+++ sanitize.go
@@ -7,7 +7,15 @@ import (
 	"strings"
 )
 
-func FindDOB(input string) (dates []string){
+func RedactDateOfBirth(input string) string {
+	foundDOB := FindDOB(input)
+	for _, DOB := range foundDOB {
+		input = strings.ReplaceAll(input, DOB, "[DOB REDACTED]")
+	}
+	return input
+}
+
+func FindDOB(input string) (dates []string) {
 	dash := regexp.MustCompile(`\d{1,2}-\d{1,2}-\d{4}`)
 	slash := regexp.MustCompile(`\d{1,2}/\d{1,2}/\d{4}`)
 	standardUS := regexp.MustCompile(`[a-zA-Z]{3,9} \d{1,2}, \d{4}`)
@@ -21,84 +29,170 @@ func FindDOB(input string) (dates []string){
 	return dates
 }
 
-func FindEmails(input string)  (emails []string){
-	var temp string
-	var email bool
-	for i, character := range input {
-		if character ==  ' ' || character == '.'{
-			if email{
-				emails = append(emails, fmt.Sprintf("%s%c%c%c%c", temp, input[i], input[i+1], input[i+2], input[i+3]))
-				email = false
-			}
-			temp = ""
-			continue
-		}
-		if character == '@'{
-			email = true
-		}
-		temp = fmt.Sprintf("%s%c", temp, character)
-	}
-	return emails
-}
-
-func RedactDateOfBirth(input string) string {
-	foundDOB := FindDOB(input)
-	for _, DOB := range foundDOB{
-		input = strings.ReplaceAll(input, DOB, "[DOB REDACTED]")
-	}
-	return input
-}
+//////////////////////////////////////////////////////////////////
 
 func RedactEmail(input string) string {
 	foundEmails := FindEmails(input)
-	for _, email := range foundEmails{
+	for _, email := range foundEmails {
 		input = strings.ReplaceAll(input, email, "[EMAIL REDACTED]")
 	}
 	return input
 }
 
-func RedactPhone(phone string) string {
-	return  redact("TEL")
-}
-
-func RedactSSN(SSN string) string {
-	return  redact("SSN")
-}
-
-func RedactCreditCard(input string) string {
-	//Find CC and then sanitize
-	cards := FindCreditCards(input)
-	return SanitizeCreditCard(cards, input)
-}
-
-func FindCreditCards(input string) (cards []string){
+func FindEmails(input string) (emails []string) {
 	var temp string
-	for _, character := range input {
-		if character == ' ' || character == '.' {
-			lengthTemp := len(temp)
-			if lengthTemp >= 13 && lengthTemp <= 19 {
-				new := strings.ReplaceAll(temp, "-", "")
-				if _, err := strconv.Atoi(new) ; err == nil {
-				cards = append(cards, temp)
-				}
+	var email bool
+	for i, character := range input {
+		if breakNotFound(character) {
+			temp = fmt.Sprintf("%s%c", temp, character)
+		} else {
+			if email {
+				emails = append(emails, buildEmail(input, temp, i))
+				email = false
 			}
 			temp = ""
 			continue
 		}
-		temp = fmt.Sprintf("%s%c", temp, character)
+		if character == '@' {
+			email = true
+		}
+	}
+	return emails
+}
+func buildEmail(input, temp string, i int) string {
+	return fmt.Sprintf("%s%c%c%c%c", temp, input[i], input[i+1], input[i+2], input[i+3])
+}
+
+//////////////////////////////////////////////////////////////////
+
+func RedactCreditCard(input string) string {
+	cards := FindCreditCards(input)
+	return SanitizeCreditCard(cards, input)
+}
+
+func FindCreditCards(input string) (cards []string) {
+	var temp string
+	for i, character := range input {
+		if breakNotFound(character) {
+			temp = fmt.Sprintf("%s%c", temp, character)
+		} else {
+			if spaceDelimitedCandidate(input, i) {
+				temp += " "
+			} else {
+				appendCandidate(temp, &cards, 13, 19)
+				temp = ""
+			}
+		}
 	}
 	return cards
 }
 
 func SanitizeCreditCard(cards []string, input string) string {
-	for _, card := range cards{
+	for _, card := range cards {
 		new := strings.ReplaceAll(card, "-", "")
+		new = strings.ReplaceAll(new, " ", "")
 		new = fmt.Sprintf("[MASKED %s****%s]", new[:4], new[12:])
 		input = strings.ReplaceAll(input, card, new)
 	}
 	return input
 }
 
-func redact(statement string) string {
-	return fmt.Sprintf("[%s REDACTED]", statement)
+//////////////////////////////////////////////////////////////////
+
+func RedactSSN(input string) string {
+	cards := FindSSN(input)
+	return SanitizeSSN(cards, input)
+}
+
+func FindSSN(input string) (SSNs []string) {
+	var temp string
+	for i, character := range input {
+		if breakNotFound(character) {
+			temp = fmt.Sprintf("%s%c", temp, character)
+		} else {
+			if spaceDelimitedCandidate(input, i) {
+				temp += " "
+			} else {
+				appendCandidate(temp, &SSNs, 9, 11)
+				temp = ""
+			}
+		}
+	}
+	return SSNs
+}
+
+func SanitizeSSN(SSNs []string, input string) string {
+	for _, SSN := range SSNs {
+		input = strings.ReplaceAll(input, SSN,"[SSN REDACTED]")
+	}
+	return input
+}
+
+//////////////////////////////////////////////////////////////////
+
+func RedactTel(input string) string {
+	telNums := FindTel(input)
+	return SanitizeTel(telNums, input)
+}
+
+func FindTel(input string) (telNums []string) {
+	var temp string
+	for i, character := range input {
+		if breakNotFound(character) {
+			temp = fmt.Sprintf("%s%c", temp, character)
+		} else {
+			if spaceDelimitedCandidate(input, i) {
+				temp += " "
+			} else {
+				appendCandidate(temp, &telNums, 10, 16)
+				temp = ""
+			}
+		}
+	}
+	return telNums
+}
+
+func SanitizeTel(telNums []string, input string) string {
+	for _, num := range telNums {
+		input = strings.ReplaceAll(input, num,"[TEL REDACTED]")
+	}
+	return input
+}
+
+//////////////////////////////////////////////////////////////////
+
+func RedactAll(input string) string{
+	input = RedactEmail(input)
+	input = RedactDateOfBirth(input)
+	input = RedactTel(input)
+	input = RedactSSN(input)
+	input = RedactCreditCard(input)
+	return input
+}
+
+func breakNotFound(character int32) bool {
+	return character != ' ' && character != '.' && character != ','
+}
+
+func appendCandidate(temp string, items *[]string, min, max int) {
+	lengthTemp := len(temp)
+	if lengthTemp == 11 && strings.Contains(temp, "-"){
+		return
+	}
+	if lengthTemp > 12 && (strings.Contains(temp, "(") || strings.Contains(temp, ")")){
+		return
+	}
+	if lengthTemp >= min && lengthTemp <= max { // SSN 9-11 //NUM 10-16 //CC 13-19
+		new := strings.ReplaceAll(temp, "-", "")
+		new = strings.ReplaceAll(new, " ", "")
+		new = strings.ReplaceAll(new, "(", "")
+		new = strings.ReplaceAll(new, ")", "")
+		if _, err := strconv.Atoi(new); err == nil {
+			*items = append(*items, temp)
+		}
+	}
+}
+
+func spaceDelimitedCandidate(input string, i int) bool {
+	return i+1 < len(input) && ('0' <= input[i+1] && input[i+1] <= '9') && ('0' <= input[i-1] && input[i-1] <= '9' || input[i-1] == ')')
 }
--- sanitize_test.go
+++ sanitize_test.go
@@ -6,8 +6,6 @@ import (
     "testing"
 )
 
-const TEST_TEXT1 = "Hello my name is John, my email address is john@test.com and my birthday is January 1, 2000."
-
 func TestSanitizeFixture(t *testing.T) {
     gunit.Run(new(SanitizeFixture), t)
 }
@@ -37,36 +35,39 @@ func (this *SanitizeFixture) TestRedactEmail() {
 }
 
 func (this *SanitizeFixture) TestRedactCC(){
-    input := "Hello my name is John, my Credit card number is: 1111-1111-1111-1111. My employees CC number is 1111111111111111."
-    expectedOutput := "Hello my name is John, my Credit card number is: [MASKED 1111****1111]. My employees CC number is [MASKED 1111****1111]."
+    input := "Hello my name is John, my Credit card number is: 1111-1111-1111-1111. My employees CC number is 1111111111111111 and 1111 1111 1111 1111 plus 1111111111111."
+    expectedOutput := "Hello my name is John, my Credit card number is: [MASKED 1111****1111]. My employees CC number is [MASKED 1111****1111] and [MASKED 1111****1111] plus [MASKED 1111****1]."
 
     output := RedactCreditCard(input)
+
     this.So(output, should.Resemble, expectedOutput)
 }
 
-func (this *SanitizeFixture) TestSanitizePhone() {
-    sanitized := RedactPhone("111-111-1111")
-    this.So(sanitized, should.Equal, "[TEL REDACTED]")
+func (this *SanitizeFixture) TestRedactSSN(){
+    input := "Hello my name is John, my SSN is: 111-11-1111 my employees SSN is 111111111 and 111 11 1111."
+    expectedOutput := "Hello my name is John, my SSN is: [SSN REDACTED] my employees SSN is [SSN REDACTED] and [SSN REDACTED]."
+
+    output := RedactSSN(input)
+
+    this.So(output, should.Resemble, expectedOutput)
 }
 
-func (this *SanitizeFixture) TestSanitizeSSN() {
-    sanitized := RedactSSN("111-11-1111")
-    this.So(sanitized, should.Equal, "[SSN REDACTED]")
+func (this *SanitizeFixture) TestRedactTel(){
+    input := "Hello my name is John, my number is: 1(801) 111-1111 and (111)111 1111 also 1111111111 one more 1-801-111-1111."
+    expectedOutput := "Hello my name is John, my number is: [TEL REDACTED] and [TEL REDACTED] also [TEL REDACTED] one more [TEL REDACTED]."
+
+    output := RedactTel(input)
+
+    this.So(output, should.Resemble, expectedOutput)
 }
 
-func (this *SanitizeFixture) SkipTestSanitizeCreditCardBasic() {
-    sanitized := RedactCreditCard("1111111111111111")
-    this.So(sanitized, should.Equal, "[MASKED 1111****1111]")
-}
+func (this *SanitizeFixture) TestRedactAll(){
+   input := "Hello my name is John, my email address is john@test.com. My phone-number is 1(111) 111-1111, my birthday is 1/11/1111, and my CC is 1111-1111-1111-1111."
+   expectedOutput := "Hello my name is John, my email address is [EMAIL REDACTED]. My phone-number is [TEL REDACTED], my birthday is [DOB REDACTED], and my CC is [MASKED 1111****1111]."
 
-func (this *SanitizeFixture) SkipTestSanitizeCreditCardDashes() {
-    sanitized := RedactCreditCard("1111-1111-1111-1111")
-    this.So(sanitized, should.Equal, "[MASKED 1111****1111]")
-}
+   output := RedactAll(input)
 
-func (this *SanitizeFixture) SkipTestSanitizeCreditCardSpaces() {
-    sanitized := RedactCreditCard("1111 1111 1111 1111")
-    this.So(sanitized, should.Equal, "[MASKED 1111****1111]")
+   this.So(output, should.Resemble, expectedOutput)
 }
 
 

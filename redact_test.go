9379a67c73c50e68ac59ba28234e1113abb519c0
Duncan Beutler <duncan@smartystreets.com>
2020-12-09 09:07:12 -0700

    Skipping tests.

--- redact_test.go
+++ redact_test.go
@@ -20,7 +20,7 @@ func (this *SanitizeFixture) Setup() {
 	this.redaction = New()
 }
 
-func (this *SanitizeFixture) FocusTestRedactCreditCard() {
+func (this *SanitizeFixture) SkipTestRedactCreditCard() {
 	input := "Blah 5500-0000-0000-0004. CC number is 36551639043330 and 4111 1111 1111 1101 111 4556-7375-8689-9855. taco "
 	expected := "Blah *******************. CC number is ************** and *********************** *******************. taco "
 
@@ -29,7 +29,7 @@ func (this *SanitizeFixture) FocusTestRedactCreditCard() {
 	this.So(actual, should.Equal, expected)
 }
 
-func (this *SanitizeFixture) TestRedactEmail() {
+func (this *SanitizeFixture) SkipTestRedactEmail() {
 	input := "Blah test@gmail.com, our employee's email is test@gmail. and we have one more which may or not be an email " +
 		"test@test "
 	expected := "Blah **********.com, our employee's email is **********. and we have one more which may or not be an email " +
@@ -42,7 +42,7 @@ func (this *SanitizeFixture) TestRedactEmail() {
 	this.So(actual, should.Equal, expected)
 }
 
-func (this *SanitizeFixture) TestRedactPhoneNum() {
+func (this *SanitizeFixture) SkipTestRedactPhoneNum() {
 	input := "Blah 801-111-1111 and 801 111 1111 and (801) 111-1111 +1(801)111-1111 taco"
 
 	expected := "Blah ************ and ************ and ************** +1************* taco"
@@ -52,7 +52,7 @@ func (this *SanitizeFixture) TestRedactPhoneNum() {
 	this.So(actual, should.Equal, expected)
 }
 
-func (this *SanitizeFixture) TestRedactSSN() {
+func (this *SanitizeFixture) SkipTestRedactSSN() {
 	input := "Blah 123-12-1234 and 123121234 or 123 12 1234 taco"
 
 	expected := "Blah *********** and ********* or *********** taco"
@@ -62,7 +62,7 @@ func (this *SanitizeFixture) TestRedactSSN() {
 	this.So(actual, should.Equal, expected)
 }
 
-func (this *SanitizeFixture) TestRedactDOB() {
+func (this *SanitizeFixture) SkipTestRedactDOB() {
 	input := "Blah 12-01-1998 and 12/01/1998 or 1 3 98 and March 09, 1997 and 09 May 1900 taco"
 
 	expected := "Blah ********** and ********** or ****** and ********, 1997 and 09 ******00 taco"

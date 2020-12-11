4f5d91675bc9533c91abf8d769ed44cd4e3884ab
Duncan Beutler <duncan@smartystreets.com>
2020-12-11 11:29:16 -0700

    Cleaning up a whole bunch of TODOs. Inlined Luhn Check.

--- redact.go
+++ redact.go
@@ -1,15 +1,18 @@
 package redact
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
 type Redaction struct {
-	used    map[int]struct{}
+	used    []bool
 	matches []match
 }
 
 func New() *Redaction {
 	return &Redaction{
-		used:    make(map[int]struct{}, 128),
+		used:    make([]bool, 256),
 		matches: make([]match, 0, 16),
 	}
 }
@@ -25,14 +28,12 @@ func (this *Redaction) All(input string) string {
 	this.clear()
 	return result
 }
-
 func (this *Redaction) clear() {
 	this.matches = this.matches[0:0]
-	for key, _ := range this.used {
-		delete(this.used, key)
+	for i := range this.used {
+		this.used[i] = false
 	}
 }
-
 func (this *Redaction) redactMatches(input string) string {
 	if len(this.matches) == 0 {
 		return input // no changes to redact
@@ -59,7 +60,7 @@ func (this *Redaction) redactMatches(input string) string {
 	return string(buffer)
 }
 
-func (this *Redaction) matchCC(input string){
+func (this *Redaction) matchCC(input string) {
 	var lastDigit int
 	var length int
 	var numbers int
@@ -68,23 +69,27 @@ func (this *Redaction) matchCC(input string){
 	var total int
 	for i := len(input) - 1; i > 0; i-- {
 		character := input[i]
+		strChar := fmt.Sprintf("%c", character)
+		_ = strChar
 
 		if !isNumeric(input[i]) {
-			if numbers > 12 && total % 10 == 0 {
-				this.appendMatch(lastDigit-length + 1, length)
+			if numbers > 12 && total%10 == 0 {
+				this.appendMatch(lastDigit-length+1, length)
 				length = 0
 				total = 0
-				isOdd = false // TODO
+				isOdd = false
 				lastDigit = i - 1
 				numbers = 0
-				isCandidate = false
 				continue
 			}
 
-			if breakNotFound(character) && !isNumeric(input[i-1]) {
+			if ccBreakNotFound(character) && !isNumeric(input[i-1]) {
 				lastDigit = i - 1
 				length = 0
 				isCandidate = false
+				continue
+			}
+			if i < len(input)-1 && !ccBreakNotFound(input[i+1]) {
 				continue
 			}
 			length++
@@ -111,95 +116,25 @@ func (this *Redaction) matchCC(input string){
 	}
 }
 
-func (this *Redaction) matchCreditCard(input string){
-	var start int
-	var length int
-	var isCandidate bool
-	var card []int
-	for i := 0; i < len(input)-1; i++ {
-		character := input[i]
-		if !isNumeric(character) {
-			// TODO: inline isCreditCard and checkLuhn into method--this avoids having to create a string to ask if it's a credit card
-			// instead we track each numeric digit here and run a tally as we go along
-
-			// TODO: Iterate backwards to be able to inline luhnfun
-			if checkCard(length, card) {
-				this.appendMatch(start, length)
-				length = 0
-				start = i + 1
-				card = card[0:0]
-				continue
-			}
-			if breakNotFound(character) && !isNumeric(input[i+1]) {
-				start = i + 1
-				length = 0
-				isCandidate = false
-				card = card[0:0]
-				continue
-			}
-			length++
-		} else {
-			number := character - '0'
-			card = append(card, int(number))
-			if isCandidate {
-				length++
-			} else {
-				isCandidate = true
-				start = i
-			}
-		}
-	}
-	if isNumeric(input[len(input)-1]) {
-		//length++ // TODO: test coverage
-	}
-	if checkCard(length, card) {
-		// matches = appendMatches(matches, start, length) // TODO: test coverage
-	}
-
-}
-func checkCard(length int, card []int) bool {
-	return length >= 13 && length <= 24 && luhnCheck(card)
-}
-
-func luhnCheck(card []int) bool {
-	var total int
-	var isOdd bool
-	for i := len(card) - 1; i >= 0; i-- {
-		isOdd = !isOdd
-		num := card[i]
-		if !isOdd {
-			num = card[i] * 2
-			if num > 9 {
-				num -= 9
-			}
-		}
-		total += num
-	}
-	return total%10 == 0
-}
-
 func (this *Redaction) matchEmail(input string) {
 	var start int
 	var length int
-	var isCandidate bool
 	for i := 0; i < len(input); i++ {
 		character := input[i]
-		if _, found := this.used[i]; found {
+		if this.used[i] {
 			continue
 		}
-		if !breakNotFound(character) {
-			if isCandidate {
-				this.appendMatch(start, length)
-			}
+		if !emailBreakNotFound(character) {
 			start = i + 1
 			length = 0
-			isCandidate = false
 			continue
 		} else {
-			length++
 			if character == '@' {
-				isCandidate = true
+				this.appendMatch(start, length)
+				start = i + 1
+				length = 0
 			}
+			length++
 		}
 	}
 }
@@ -209,7 +144,7 @@ func (this *Redaction) matchPhone(input string) {
 	var length int
 	var isCandidate bool
 	for i := 0; i < len(input)-1; i++ {
-		if _, found := this.used[i]; found {
+		if this.used[i] {
 			continue
 		}
 		character := input[i]
@@ -257,7 +192,7 @@ func (this *Redaction) matchSSN(input string) {
 	var isCandidate bool
 	for i := 0; i < len(input)-1; i++ {
 		character := input[i]
-		if _, found := this.used[i]; found {
+		if this.used[i] {
 			continue
 		}
 		if !isNumeric(character) {
@@ -267,7 +202,7 @@ func (this *Redaction) matchSSN(input string) {
 				start = i + 1
 				continue
 			}
-			if breakNotFound(character) {
+			if ssnBreakNotFound(character) {
 				start = i + 1
 				length = 0
 				isCandidate = false
@@ -299,14 +234,15 @@ func (this *Redaction) matchDOB(input string) {
 	var monthStart int
 	var monthLength int
 	var monthCandidate bool
+	//var startChar byte
 
 	for i := 0; i < len(input)-1; i++ {
 		character := input[i]
-		if _, found := this.used[i]; found {
+		if this.used[i] {
 			continue
 		}
 		if !isNumeric(character) {
-			if breakNotFound(character) {
+			if dobBreakNotFound(character) {
 				monthLength++
 				start = i + 1
 				length = 0
@@ -314,12 +250,17 @@ func (this *Redaction) matchDOB(input string) {
 				continue
 			}
 			// TODO: this is allocating another string. Don't do dat.
-			// Instead, pass in the starting index and length and found out if it's a month
+			// Instead, pass in the starting index and length and find out if it's a month
 			if isMonth(input[monthStart : monthStart+monthLength]) {
 				monthCandidate = true
 				continue
 			}
+			//if monthCheck(startChar, character) {
+			//	monthCandidate = true
+			//	continue
+			//}
 			monthStart = i + 1
+			//startChar = input[i + 1]
 			monthLength = 0
 			if isDOB(length) {
 				this.appendMatch(start, length)
@@ -357,23 +298,28 @@ func isMonth(month string) bool {
 	_, found := months[strings.ToLower(month)]
 	return found
 }
-func isMonth2(input string, start, length int) bool {
-	_, found := months[input[start:start+length]]
-	return found
-}
 func isNumeric(value byte) bool {
 	return value >= '0' && value <= '9'
 }
 
-func breakNotFound(character byte) bool {
-	return character != '-' && character != ' ' && character != '.' && character != '/'
+func ccBreakNotFound(character byte) bool {
+	return character != '-' && character != ' '
+}
+func emailBreakNotFound(character byte) bool {
+	return character != '.' && character != ' '
 }
 func phoneBreakNotFound(character byte) bool {
-	return breakNotFound(character) && character != '(' && character != ')'
+	return character != '-' && character != ' ' && character != '(' && character != ')'
+}
+func ssnBreakNotFound(character byte) bool {
+	return character != '-' && character != ' '
+}
+func dobBreakNotFound(character byte) bool {
+	return character != '-' && character != ' ' && character != '/'
 }
 func (this *Redaction) appendMatch(start, length int) {
 	for i := start; i <= start+length; i++ {
-		this.used[i] = struct{}{}
+		this.used[i] = true
 	}
 
 	this.matches = append(this.matches, match{InputIndex: start, Length: length})
--- redact_test.go
+++ redact_test.go
@@ -20,29 +20,27 @@ func (this *SanitizeFixture) Setup() {
 	this.redaction = New()
 }
 
-func (this *SanitizeFixture) SkipTestRedactCreditCard() {
-	input := "Blah 5500-0000-0000-0004. CC number is 36551639043330 and 4111 1111 1111 1101 111 4556-7375-8689-9855. taco "
-	expected := "Blah *******************. CC number is ************** and *********************** *******************. taco "
+func (this *SanitizeFixture) TestRedactCreditCard() {
+	input := "Blah 5500-0000-0000-0004. CC number is 36551639043330  and 4111 1111 1111 1101 111 4556-7375-8689-9855. taco "
+	expected := "Blah *******************. CC number is **************  and *********************** *******************. taco "
 
 	actual := this.redaction.All(input)
 
 	this.So(actual, should.Equal, expected)
 }
 
-func (this *SanitizeFixture) SkipTestRedactEmail() {
+func (this *SanitizeFixture) TestRedactEmail() {
 	input := "Blah test@gmail.com, our employee's email is test@gmail. and we have one more which may or not be an email " +
-		"test@test "
-	expected := "Blah **********.com, our employee's email is **********. and we have one more which may or not be an email " +
-		"********* "
-	// TODO: Test with taco
-	// TODO: No space
+		"test@test taco"
+	expected := "Blah ****@gmail.com, our employee's email is ****@gmail. and we have one more which may or not be an email " +
+		"****@test taco"
 
 	actual := this.redaction.All(input)
 
 	this.So(actual, should.Equal, expected)
 }
 
-func (this *SanitizeFixture) SkipTestRedactPhoneNum() {
+func (this *SanitizeFixture) TestRedactPhoneNum() {
 	input := "Blah 801-111-1111 and 801 111 1111 and (801) 111-1111 +1(801)111-1111 taco"
 
 	expected := "Blah ************ and ************ and ************** +1************* taco"
@@ -52,7 +50,7 @@ func (this *SanitizeFixture) SkipTestRedactPhoneNum() {
 	this.So(actual, should.Equal, expected)
 }
 
-func (this *SanitizeFixture) SkipTestRedactSSN() {
+func (this *SanitizeFixture) TestRedactSSN() {
 	input := "Blah 123-12-1234 and 123121234 or 123 12 1234 taco"
 
 	expected := "Blah *********** and ********* or *********** taco"
@@ -62,7 +60,7 @@ func (this *SanitizeFixture) SkipTestRedactSSN() {
 	this.So(actual, should.Equal, expected)
 }
 
-func (this *SanitizeFixture) SkipTestRedactDOB() {
+func (this *SanitizeFixture) TestRedactDOB() {
 	input := "Blah 12-01-1998 and 12/01/1998 or 1 3 98 and March 09, 1997 and 09 May 1900 taco"
 
 	expected := "Blah ********** and ********** or ****** and ********, 1997 and 09 ******00 taco"

a35d228f1d71848d2359068d9dea692464e41f36
Caroline Hickey <caroline@smartystreets.com>
2021-07-12 15:04:30 -0600

    Changed Credit card algorithm

--- credit.go
+++ credit.go
@@ -1,34 +1,38 @@
 package redact
 
-import "strings"
-
-
+import (
+	"fmt"
+	"strings"
+)
 
 func (this *creditCardRedact) match(input []byte) {
 	if len(input) <= 0 {
 		return
 	}
+
 	//input := 'Hi, my name is 24601 Caroline. I like to sing 4024007103939509 in choirs.'
+	//First, take a character and check to see if you care about it.
+	//Second, if you do add it to the overall value
+	// --> Go look at Savannah's reset
+	// third check to see if the value passes the luhn algorithm?
 	for i := 0; i < len(input); i++ {
 		character := input[i]
 
-		if isInterestingValue(character, input, i) {
-			this.value = append(this.value, character)
-
+		if this.isInterestingValue(character) {
 			if len(this.value) == 1 {
-				this.lastDigitIndex = i - 1
+				this.lastDigitIndex = i
 			}
-			if !isInterestingValue(character, input, i) && !luhn(stripBreaks(this.value)) {
-				this.value = nil
-			} else if !isNumeric(input[i+1]) && luhn(string(this.value[:])) {
-				this.appendMatch(this.lastDigitIndex, len(this.value))
-				this.value = nil
-			}
+			continue
+		}
+		fmt.Println(this.value)
+		if luhn(this.value){
+			this.appendMatch(this.lastDigitIndex, len(this.value))
+			//reset the values
 		}
 	}
 }
 
-func luhn(CardNumber string) bool {
+func luhn(CardNumber []byte) bool {
 	odd := len(CardNumber) & 1
 	var sum int
 	for i, c := range CardNumber {
@@ -46,13 +50,15 @@ func luhn(CardNumber string) bool {
 
 var t = [...]int{0, 2, 4, 6, 8, 1, 3, 5, 7, 9}
 
-func isInterestingValue(value byte, input []byte, i int) bool{
+func(this *creditCardRedact) isInterestingValue(value byte) bool {
 	interesting := false
-	if value >= '0' && value <= '9'{
+	switch {
+	case value == '-':
 		interesting = true
-	}
-	if (value == ' ' || value == '-') && (input[i - 1] >= '0' && input[i - 1] <='9'){
+	case value == ' ':
 		interesting = true
+	case isNumeric(value):
+		this.value = append(this.value, value)
 	}
 	return interesting
 }

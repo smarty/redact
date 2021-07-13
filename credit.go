8d69cbe82b2480b15a685f2ebfbae932d796942d
Caroline Hickey <caroline@smartystreets.com>
2021-07-13 13:18:11 -0600

    changed algorithm on credit card

--- credit.go
+++ credit.go
@@ -1,17 +1,12 @@
 package redact
 
-import (
-	"fmt"
-	"strings"
-)
-
 func (this *creditCardRedact) match(input []byte) {
 	if len(input) <= 0 {
 		return
 	}
 
 	//input := 'Hi, my name is 24601 Caroline. I like to sing 4024007103939509 in choirs.'
-	//First, take a character and check to see if you care about it.
+	//First, take a character and check to see if you care about it. (if it is a number or a break)
 	//Second, if you do add it to the overall value
 	// --> Go look at Savannah's reset
 	// third check to see if the value passes the luhn algorithm?
@@ -19,15 +14,18 @@ func (this *creditCardRedact) match(input []byte) {
 		character := input[i]
 
 		if this.isInterestingValue(character) {
-			if len(this.value) == 1 {
+			if len(this.value) == 1{
 				this.lastDigitIndex = i
 			}
 			continue
 		}
-		fmt.Println(this.value)
-		if luhn(this.value){
+		//if luhn(this.value){
+		//	this.appendMatch(this.lastDigitIndex, len(this.value))
+		//	this.resetValues(i)
+		//}
+		if len(this.value) >= 19 {
 			this.appendMatch(this.lastDigitIndex, len(this.value))
-			//reset the values
+			this.resetValues(i)
 		}
 	}
 }
@@ -50,7 +48,7 @@ func luhn(CardNumber []byte) bool {
 
 var t = [...]int{0, 2, 4, 6, 8, 1, 3, 5, 7, 9}
 
-func(this *creditCardRedact) isInterestingValue(value byte) bool {
+func (this *creditCardRedact) isInterestingValue(value byte) bool {
 	interesting := false
 	switch {
 	case value == '-':
@@ -63,9 +61,8 @@ func(this *creditCardRedact) isInterestingValue(value byte) bool {
 	return interesting
 }
 
-func stripBreaks(input []byte) string {
-	returnString := string(input[:])
-	strings.ReplaceAll(returnString, " ", "")
-	strings.ReplaceAll(returnString, "-", "")
-	return returnString
+func (this *creditCardRedact) resetValues(i int) {
+	this.value = nil
+	this.breakType = byte(0)
+	this.lastDigitIndex = i + 1
 }
